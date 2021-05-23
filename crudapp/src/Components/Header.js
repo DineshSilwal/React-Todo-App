8b93755fb35eb8b0e4bbd339bbfa6c0d1365889f
DineshSilwal <dnessilwal04G@gmail.com>
2021-05-23 19:44:45 +0545

    inital commit : add components

--- crudapp/src/Components/Header.js
+++ crudapp/src/Components/Header.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React from 'react'
+import {Link} from "react-router-dom";
 
-export const Header = () => {
+export  const  Header = (props) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">TODO-LIST</a>
+                <Link className="navbar-brand" to="/">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
@@ -13,19 +14,18 @@ export const Header = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">About</a>
+                            <Link className="nav-link" to="/about">About</Link>
                         </li>
                     </ul>
-                    <form className="d-flex">
+                    {props.searchBar ? <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                         <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                    </form> : ""}
                 </div>
             </div>
         </nav>
     )
-
 }
